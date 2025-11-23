0be6025b45812d82b4f15242b583381c4c0213e8
tanyarai5446 <tr5335074@gmail.com>
2025-11-23 12:47:14 +0530

    number guessing game

--- NumberGuess.java
+++ NumberGuess.java
@@ -6,22 +6,21 @@ public class NumberGuess {
         Random rd=new Random();
         int guess;
         int attempts=0;
-        int randomNum=rd.nextInt(1,100);
+        int randomNum=rd.nextInt(1,10);
         Scanner sc=new Scanner(System.in);
         System.out.println("Number guessing game");
         System.out.print("Guess a number between 1-100 ");
         do{
             System.out.print("\nEnter a guess: ");
             guess=sc.nextInt();
-            if(guess<randomNum){
-                System.out.print("Number too short. ");
-            System.out.print("Try again!");}
-            else if(guess>randomNum){
-                System.out.print("Number too long. ");
-                System.out.print("Try again!");}
+            if(guess<randomNum)
+                System.out.print("Number too short. Try again!");
+            else if(guess>randomNum)
+                System.out.print("Number too long. Try again!");
+            else
+                System.out.print("Correct! Hurray, You won");
             attempts++;
         }while(guess!=randomNum);
-        System.out.println("\nYou have won ");
-        System.out.print("You took "+attempts+" attempts to guess the number.");
+        System.out.print("\nYou took "+attempts+" attempts to guess the number.");
     }
 }
