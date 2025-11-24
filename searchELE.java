e2f7a456a5f3fd97cb8a266df61f2aedb184ee71
tanyarai5446 <tr5335074@gmail.com>
2025-11-24 19:51:08 +0530

    modified searching an element (in strings)

--- searchELE.java
+++ searchELE.java
@@ -2,20 +2,22 @@ import java.util.Scanner;
 public class searchELE {
     public static void main(String[]args){
         int[] numbers={1,2,5,8,3};
+        String[] foods={"apple","orange","banana"};
         System.out.print("Enter target: ");
         Scanner sc = new Scanner(System.in);
-        int t=sc.nextInt();
+        String t=sc.nextLine();
         boolean isfound=false;
-        for(int i=0;i<numbers.length;i++)
+        for(int i=0;i<foods.length;i++)
         {
-            if(t==numbers[i])
+            //if(t==numbers[i])
+            if(foods[i].equals(t))//in case of string we use equals as using == may store memory address
             {
-                System.out.print("element found at index "+i);
+                System.out.print("fruit found at index "+i);
                 isfound=true;
                 break;
             }
         }
         if(!isfound){
-            System.out.print("element not found");}
+            System.out.print("fruit not found");}
     }
 }
