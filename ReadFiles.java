dd4826252fea67fcc466e5f19427b61d08252bac
tanyarai5446 <tr5335074@gmail.com>
2025-12-20 10:26:00 +0530

    reading files (1)in java

--- ReadFiles.java
+++ ReadFiles.java
@@ -5,7 +5,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 public class ReadFiles {
     public static void main(String[]args) {
-        //BufferedReader=best fo reading txt files
+        //BufferedReader=best fo reading txt files         👈
         //FileInputStream=best for binary files(images,audio files)
         //RandomAccessFile=best for read/write specific portions of large files
         try (BufferedReader reader = new BufferedReader(new FileReader("text.txt"))) {
